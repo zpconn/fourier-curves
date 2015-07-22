6f8f3231bd91ed614b2771fa640e77a98ab96176
Zach Conn <zpconn@gmail.com>
2015-07-21 19:49:49 -0500

    It actually works now.

--- curves.js
+++ curves.js
@@ -3,7 +3,9 @@ var margin = {top: 0,
 			  bottom: 0,
 			  left: 0},
 	width = window.innerWidth - margin.left - margin.right,
-	height = window.innerHeight - margin.top - margin.bottom;
+	height = window.innerHeight - margin.top - margin.bottom,
+	screenCenterX = window.innerWidth / 2,
+	screenCenterY = window.innerHeight / 2;
 
 var x = d3.scale.linear()
 	.domain([0, width])
@@ -16,6 +18,10 @@ var y = d3.scale.linear()
 var line = d3.svg.line()
 	.x(function(d, i) { return x(d.re); })
 	.y(function(d, i) { return y(d.im); });
+
+var fourierLine = d3.svg.line()
+	.x(function(d, i) { return x(d.re) + screenCenterX; })
+	.y(function(d, i) { return y(d.im) + screenCenterY; });
 
 var svg = d3.select("body").append("svg")
 	.attr("width", width + margin.left + margin.right)
@@ -38,11 +44,20 @@ var fourierPath = svg.append("path")
 	.attr("class", "fourierLine")
 	.attr("d", line);
 
+var time = 0.0,
+	timeStep = 0.001,
+	alreadyAnimating = false;
+
+var savedCoefficients = [];
+
+var dots = [],
+	circles = [];
+
 function mouseClick() {
 	var clickCoords = d3.mouse(this),
 		complexClickCoords = math.complex(clickCoords[0], clickCoords[1]);
 
-	complexInputPoints.push(complexClickCoords);
+	complexInputPoints.push(math.complex(complexClickCoords.re - screenCenterX, complexClickCoords.im - screenCenterY));
 	pathData.push(complexClickCoords);
 	path.attr("d", line);
 
@@ -66,7 +81,73 @@ function mouseClick() {
 	fourierPath.transition()
 		       .duration(50)
 			   .ease("linear")
-			   .attr("d", line);
+			   .attr("d", fourierLine);
+
+	savedCoefficients = coefficients;
+	createAssemblage(coefficients);
+
+	if (!alreadyAnimating) {
+		setTimeout(animate, 500);
+		alreadyAnimating = true;
+	}
+}
+
+function createAssemblage(coefficients) {
+	var positions = computeAssemblagePositions(coefficients, 0),
+		prevPosition = math.complex(0.0, 0.0),
+		idx = -Math.floor(complexInputPoints.length/2) + 1;
+
+	while (dots.length > 0) {
+		var dot = dots.pop();
+		dot.remove();
+	}
+
+	while (circles.length > 0) {
+		var circle = circles.pop();
+		circle.remove();
+	}
+
+	positions.forEach(function (p) {
+		var dot = svg.append("circle")
+			.attr("class", "dot")
+			.attr("r", 1)
+			.attr("transform", "translate(" + [p.re + screenCenterX, p.im + screenCenterY] + ")");
+
+	    dots.push(dot);
+
+		var coefficient = coefficients[idx],
+		    magnitude = Math.sqrt(coefficient.re * coefficient.re + coefficient.im * coefficient.im);
+
+		var circle = svg.append("circle")
+			.attr("class", "circle")
+			.attr("r", magnitude)
+			.attr("transform", "translate(" + [prevPosition.re + screenCenterX, prevPosition.im + screenCenterY] + ")");
+
+	    circles.push(circle);
+
+		prevPosition = p;
+		++idx;
+	});
+}
+
+function animate() {
+	var positions = computeAssemblagePositions(savedCoefficients, time),
+		idx = 0;
+
+	positions.forEach(function (p) {
+		var dot = dots[idx];
+		dot.attr("transform", "translate(" + [p.re + screenCenterX, p.im + screenCenterY] + ")");
+
+		if (idx < positions.length - 1) {
+			var circle = circles[idx + 1];
+			circle.attr("transform", "translate(" + [p.re + screenCenterX, p.im + screenCenterY] + ")");
+		}
+
+		++idx;
+	});
+
+	time += timeStep;
+	setTimeout(animate, 5);
 }
 
 function fourierCoefficients(complexPoints, numPairs) {
@@ -93,6 +174,27 @@ function fourierCoefficients(complexPoints, numPairs) {
 	}
 
 	return coefficients;
+}
+
+function computeAssemblagePositions(coefficients, t) {
+	var positions = [],
+		sum = math.complex(0.0, 0.0),
+		idx = 0;
+
+	for (var n = -Math.floor(complexInputPoints.length/2) + 1; n <= Math.floor(complexInputPoints.length/2) - 1; ++n) {
+		var exponential = math.exp(math.chain(2.0)
+				                       .multiply(Math.PI)
+									   .multiply(math.complex(0.0, 1.0))
+									   .multiply(n)
+									   .multiply(t)
+									   .done());
+
+		sum = math.add(sum, math.multiply(coefficients[n], exponential));
+		positions[idx] = sum;
+		idx += 1;
+	}
+
+	return positions;
 }
 
 function complexFourierPoints(coefficients) {
